efae93f717d17882773ddc2d25fa97f4959e48e9
Sienna Folkes-Miller <sfolkesmiller@gmail.com>
2025-10-11 16:21:25 +0100

    Data imported

--- test file.py
+++ test file.py
@@ -1,4 +1,12 @@
-print('Testing')
-def function():
-    x=3
-    return x
+import pandas as pd
+
+df = pd.read_csv('supernovae_data.csv', delim_whitespace=True, comment='#')           
+print(df)
+
+split_line = 45
+low_redshift = df.iloc[:split_line]
+high_redshift = df.iloc[split_line:]
+
+low_redshift_values = low_redshift[['redshift', 'm_effective', 'error']].values  
+
+high_redshift_values = high_redshift[['redshift', 'm_effective', 'error']].values  
