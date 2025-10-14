084b489a7d9f408ec720552450b38d440a4d2f52
Sienna Folkes-Miller <sfolkesmiller@gmail.com>
2025-10-14 21:30:21 +0100

    luminosity first attempt

--- data_splitting.py
+++ data_splitting.py
@@ -4,9 +4,11 @@ df = pd.read_csv('supernovae_data.csv', delim_whitespace=True, comment='#')
 print(df)
 
 split_line = 45
-low_redshift = df.iloc[:split_line]
-high_redshift = df.iloc[split_line:]
+high_redshift = df.iloc[:split_line]
+low_redshift = df.iloc[split_line:]
 
 low_redshift_values = low_redshift[['redshift', 'm_effective', 'error']].values  
 
 high_redshift_values = high_redshift[['redshift', 'm_effective', 'error']].values  
+
+print(low_redshift_values[1])
