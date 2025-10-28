b6e59cee6ccc10b7f83908b299f55d2ebf0aca9e
Sienna Folkes-Miller <sfolkesmiller@gmail.com>
2025-10-28 18:09:20 +0000

    Minor adjustments

--- part2_results.py
+++ part2_results.py
@@ -7,7 +7,7 @@ from part2_defs import luminosity_distance
 from part2_defs import m_model
 from part2_defs import chi_squared
 
-Omega_L_values = np.linspace(0.5, 1, 500) 
+Omega_L_values = np.linspace(0.1, 1, 500) 
 
 #runs chi squared minimisation over given L values
 chi2_values = np.array([chi_squared(O, high_redshift_values)[0] for O in Omega_L_values])
@@ -25,15 +25,11 @@ Omega_L_high = np.interp(chi2_threshold, chi2_values[best_index:], Omega_L_value
 sigma_OL_minus = best_Omega_L - Omega_L_low
 sigma_OL_plus = Omega_L_high - best_Omega_L
 average_error = (sigma_OL_minus + sigma_OL_plus) / 2
-print(best_Omega_L)
-print(sigma_OL_minus, sigma_OL_plus)
-print(average_error)
 
 #editing s.f. and d.p. for L and its error
-error_rounded = float(f"{average_error:.2g}")
-decimal_places = -int(np.floor(error_rounded))
+error_rounded = float(f"{average_error:.1g}") 
+decimal_places = -int(np.floor(np.log10(error_rounded)))
 Omega_L_rounded = round(best_Omega_L, decimal_places)
-print(error_rounded:.{decimal_places}f)
 
 print(f"Best-fit Omega L = ({Omega_L_rounded:.{decimal_places}f} ± {error_rounded:.{decimal_places}f})")
 print(f"Minimum chi-squared = {chi2_min}")
